da0afef3c8d8382a007267af52e35f8b13b29336
Juan Mendoza <juan@informatica.yaracuy.yaracuy.gob.ve>
2017-06-30 16:48:41 -0400

    Modificando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 let gulp = require('gulp')
 let sass = require('gulp-sass')
 let rename = require('gulp-rename')
+let babel = require('babelify')
+let browserify = require('browserify')
 
 gulp.task('styles',function(){
 	gulp
@@ -14,6 +16,13 @@ gulp.task('assets',function(){
 	gulp
 		.src('assets/*')
 		.pipe(gulp.dest('public'))
+})
+
+gulp.task('scripts',function(){
+	browserify('./src/index.js')
+		.transform(babel)
+		.bundle()
+		.pipe()
 })
 
 gulp.task('default',['styles','assets'])
