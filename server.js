ebf4632681fd58f444c15d231fad07db8623918e
Juan Mendoza <juan@informatica.yaracuy.yaracuy.gob.ve>
2017-06-30 16:51:41 -0400

    Subiendo Modificaciones

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let app = express()
 
 app.set('view engine','pug')
 
-app.use('/public',express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.render('index')
@@ -14,8 +14,6 @@ app.get('/', function (req, res) {
 
 app.listen(3000,function(err){
 	if (err) return console.log('Hubo un Error',process.exit(1))
-
-	console.log(__dirname)
 
 	console.log('Platzigram Escuchando en el puerto 3000')
 })
